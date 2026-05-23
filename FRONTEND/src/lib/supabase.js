616008450ad6a04f5dd397b6997b7519064ad5c6
jeki18ros-gif <jeki18ros@gmail.com>
2026-05-22 21:08:29 -0500

    feat: :fire: seguridad

--- FRONTEND/src/lib/supabase.js
+++ FRONTEND/src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://pvitqvrwaqxghakonmiy.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2aXRxdnJ3YXF4Z2hha29ubWl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNzg1ODYsImV4cCI6MjA5MjY1NDU4Nn0.2TmVP82N4w1ohI3kLJ7yJpa2VQ5BPKkk4hdjOiZEGO4';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
